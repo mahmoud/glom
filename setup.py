9b71f21e6880365ae11a791ccd8c099a577a9a51
Mahmoud Hashemi <mahmoud@hatnote.com>
2018-04-25 11:02:27 -0700

    bump for 0.0.3 release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 
 __author__ = 'Mahmoud Hashemi'
-__version__ = '0.0.3dev'
+__version__ = '0.0.3'
 __contact__ = 'mahmoud@hatnote.com'
 __url__ = 'https://github.com/mahmoud/glom'
 __license__ = 'BSD'
