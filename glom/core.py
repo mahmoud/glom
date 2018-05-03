47393d3d307f60b6d6ddef5596b59e555fdd0811
Mahmoud Hashemi <mahmoud@hatnote.com>
2018-05-02 23:31:02 -0700

    glom cli notes

--- glom/core.py
+++ glom/core.py
@@ -471,16 +471,35 @@ register = _DEFAULT.register
 
 
 def main(argv):
+    """glom.py [FLAGS]] <spec> <target>
+    TODO:
+
+    --unsafe  (does a full eval() of spec, with many glom builtins available)
+    --target-file <path> (can be -)
+    --target-format  (anything other than json out there?)
+    --indent <num_of_spaces> (defaults to 2)
+    --debug (Inspect post mortem)
+    --inspect  (Inspect breakpoint)
+
+    if target-file is not set, target is either what's on the command
+    line, or stdin.
+    (i.e., if not os.isatty(sys.stdin.fileno()): sys.stdin.read()
+    or something)
+
+    results come out on stdout, but errors and any trace/debug info
+    should be on stderr, at least in CLI mode.
+
+    """
     print(argv)
     spec_text = argv[1]
     target_text = argv[2]
 
-    # TODO --unsafe
     import ast
     if not spec_text:
         spec = Path()
     else:
         if spec_text[0] not in ('"', "'", "[", "{", "("):
+            # intention: handle trivial path access
             spec_text = '"' + spec_text + '"'
         spec = ast.literal_eval(spec_text)
 
