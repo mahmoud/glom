1e24a7c2b7c0343fb5314630955da0c4d0f799c7
Mahmoud Hashemi <mahmoud@hatnote.com>
2018-07-04 14:10:15 -0700

    clean up basic check tests

--- glom/test/test_check.py
+++ glom/test/test_check.py
@@ -1,26 +1,30 @@
 
+from pytest import raises
+
 from glom import glom, Check, GlomCheckError, Coalesce, OMIT, T
 
 
 def test_check_basic():
-    def _err(f):
-        try:
-            f()
-            assert False  # pragma: no cover
-        except GlomCheckError:
-            pass
+    assert glom([0, OMIT], [T]) == [0]  # sanity check OMIT
+
     target = [{'id': 0}, {'id': 1}]
-    assert glom([0, OMIT], [T]) == [0]
     assert glom(target, ([Coalesce(Check('id', equal_to=0), default=OMIT)], T[0])) == {'id': 0}
     assert glom(target, ([Check('id', equal_to=0, default=OMIT)], T[0])) == {'id': 0}
-    assert glom([1, 'a'], [Check(type=str, default=OMIT)]) == ['a']
-    assert glom([1, 'a'], [Check(type=(str, int))]) == [1, 'a']
-    assert glom([1, 'a'], [Check(instance_of=str, default=OMIT)]) == ['a']
-    assert glom([1, 'a'], [Check(instance_of=(str, int))]) == [1, 'a']
-    _err(lambda: glom(1, Check(type=str)))
-    _err(lambda: glom(1, Check(type=(str, bool))))
-    _err(lambda: glom(1, Check(instance_of=str)))
-    _err(lambda: glom(1, Check(instance_of=(str, bool))))
-    _err(lambda: glom(1, Check(equal_to=0)))
-    _err(lambda: glom(1, Check(one_of=(0,))))
-    _err(lambda: glom(1, Check(one_of=(0, 2))))
+
+    target = [1, 'a']
+    assert glom(target, [Check(type=str, default=OMIT)]) == ['a']
+    assert glom(target, [Check(type=(str, int))]) == [1, 'a']
+    assert glom(target, [Check(instance_of=str, default=OMIT)]) == ['a']
+    assert glom(target, [Check(instance_of=(str, int))]) == [1, 'a']
+
+    failing_checks = [(1, Check(type=str)),
+                      (1, Check(type=(str, bool))),
+                      (1, Check(instance_of=str)),
+                      (1, Check(instance_of=(str, bool))),
+                      (1, Check(equal_to=0)),
+                      (1, Check(one_of=(0,))),
+                      (1, Check(one_of=(0, 2)))]
+
+    for target, check in failing_checks:
+        with raises(GlomCheckError):
+            glom(target, check)
