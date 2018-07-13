d365e50f6df06662416927bbc2064340ddf0b437
Kurt Rose <kurt@kurtrose.com>
2018-07-13 09:07:20 -0700

    even more unit tests around assign behavior

--- glom/test/test_mutable.py
+++ glom/test/test_mutable.py
@@ -12,6 +12,14 @@ def test_assign():
     assert glom(Foo(), Assign(T.a, 1)).a == 1
     assert glom({}, Assign('a', 1)) == {'a': 1}
     assert glom(Foo(), Assign('a', 1)).a == 1
+    assert glom({'a': Foo()}, Assign('a.a', 1))['a'].a == 1
+    def r():
+        r = {}
+        r['r'] = r
+        return r
+    assert glom(r(), Assign('r.r.r.r.r.r.r.r.r', 1)) == {'r': 1}
+    assert glom(r(), Assign(T['r']['r']['r']['r'], 1)) == {'r': 1}
+    assert glom(r(), Assign(Path('r', 'r', T['r']), 1)) == {'r': 1}
     with pytest.raises(TypeError):
         glom({}, Assign(1, 'a'))
     with pytest.raises(ValueError):
