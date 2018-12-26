70e63ed83d7ae11ec497186c30c974867817c19a
Mahmoud Hashemi <mahmoud@hatnote.com>
2018-12-25 21:08:05 -0800

    bumping for v18.4.1dev

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 
 __author__ = 'Mahmoud Hashemi and Kurt Rose'
-__version__ = '18.4.0'
+__version__ = '18.4.1dev'
 __contact__ = 'mahmoud@hatnote.com'
 __url__ = 'https://github.com/mahmoud/glom'
 __license__ = 'BSD'
