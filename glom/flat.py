93c33eb8db268e4cd2a355664c2c52de427ed517
Mahmoud Hashemi <mahmoud@hatnote.com>
2019-01-16 19:02:38 -0800

    Sum() pretty much ready to go

--- glom/flat.py
+++ glom/flat.py
@@ -1,31 +1,33 @@
 
 from boltons.typeutils import make_sentinel
 
-from .core import TargetRegistry, Path
+from .core import TargetRegistry, Path, T, glom
 
 _MISSING = make_sentinel('_MISSING')
 
+# TODO: Sum, Flatten, and Reduce
+
 
 class Sum(object):
-    def __init__(self, default=None):
-        self.default = default
+    def __init__(self, subspec=T, start=0):
+        self.subspec = subspec
+        self.start = start
 
     def glomit(self, target, scope):
-        ret = _MISSING
+        ret = self.start
+
+        if self.subspec is not T:
+            target = scope[glom](target, self.subspec, scope)
 
         iterate = scope[TargetRegistry].get_handler('iterate', target, path=scope[Path])
 
         try:
             iterator = iterate(target)
         except Exception as e:
+            # TODO: should this be a GlomError of some form?
             raise TypeError('failed to iterate on instance of type %r at %r (got %r)'
                             % (target.__class__.__name__, Path(*scope[Path]), e))
 
-        for val in iterator:
-            if ret is _MISSING:
-                ret = type(val)()
-                continue
-            ret += val
-        if ret is _MISSING:
-            return self.default
+        for v in iterator:
+            ret += v
         return ret
--- glom/test/test_flat.py
+++ glom/test/test_flat.py
@@ -1,6 +1,35 @@
 
-from glom import glom, Sum
+from glom import glom, Sum, Coalesce
 
-def test_sum_basic():
+def test_sum_integers():
+    target = list(range(5))
 
-    assert glom(list(range(5)), Sum()) == 10
+    assert glom(target, Sum()) == 10
+
+    assert glom(target, Sum(start=2)) == 12
+
+    target = []
+    assert glom(target, Sum()) == 0
+
+
+    target = [{"num": 3}, {"num": 2}, {"num": -1}]
+    assert glom(target, Sum(['num'])) == 4
+
+    target = target + [{}]  # add a non-compliant dict
+    assert glom(target, Sum([Coalesce('num', default=0)])) ==4
+
+
+def test_sum_seqs():
+    target = [(x,) for x in range(4)]
+    assert glom(target, Sum(start=())) == (0, 1, 2, 3)
+
+    # would not work with builtin sum(), gets:
+    # "TypeError: sum() can't sum strings [use ''.join(seq) instead]"
+    # Works here for now. If we're ok with that error, then we can
+    # switch to sum().
+    target = ['a', 'b', 'cd']
+    assert glom(target, Sum(start='')) == 'abcd'
+
+    target = [['a'], ['b'], ['cde'], ['']]
+
+    assert glom(target, Sum(Sum(start=[]), start='')) == 'abcde'
