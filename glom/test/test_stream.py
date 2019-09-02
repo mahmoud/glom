de8713edcdf4bdc376404c6e3bdb5acb69d86735
Kurt Rose <=>
2019-09-02 16:23:06 -0700

    python3 compat test

--- glom/test/test_stream.py
+++ glom/test/test_stream.py
@@ -8,7 +8,7 @@ def test_iter():
 	assert list(glom("123", Iter(int))) == [1, 2, 3]
 	cnt = count()
 	cnt_1 = glom(cnt, Iter(lambda t: t + 1))
-	assert (cnt_1.next(), cnt_1.next()) == (1, 2)
+	assert (next(cnt_1), next(cnt_1)) == (1, 2)
 	assert cnt.next() == 2
 
 	assert list(glom("123", (Iter(int), enumerate))) == [(0, 1), (1, 2), (2, 3)]
