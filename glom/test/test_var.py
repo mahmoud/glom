303c9cc469e0e0148db41cae2e771b94ecde1176
Kurt Rose <kurt@kurtrose.com>
2019-10-18 09:42:40 -0700

    cleaning up test_var a little

--- glom/test/test_var.py
+++ glom/test/test_var.py
@@ -4,6 +4,6 @@ from glom import Name, Globals, glom, T, ROOT, S, Literal
 def test_var():
     data = {'a': 1, 'b': [{'c': 2}, {'c': 3}]}
     output = [{'a': 1, 'c': 2}, {'a': 1, 'c': 3}]
-    glom(data, (Name(a=T['a']), 'b', [{'a': S['a'], 'c': 'c'}])) == output
+    assert glom(data, (Name(a=T['a']), 'b', [{'a': S['a'], 'c': 'c'}])) == output
     assert glom(data, (Globals(a=T['a']), 'b', [{'a': S['a'], 'c': 'c'}])) == output
-    glom(data, ('b', [{'a': S[ROOT][Literal(T)]['a'], 'c': 'c'}])) == output
+    assert glom(data, ('b', [{'a': S[ROOT][Literal(T)]['a'], 'c': 'c'}])) == output
