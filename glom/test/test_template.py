8515ac2f1b9bb6fc71cf17b42079ea6d31f3ac56
Kurt Rose <=>
2019-10-22 19:41:16 -0700

    test coverage

--- glom/test/test_template.py
+++ glom/test/test_template.py
@@ -5,3 +5,4 @@ def test():
     assert glom('123', Template({T[0], frozenset([T[1], T[2]])})) == {'1', frozenset(['2', '3'])}
     assert glom('xyz', Template([T[0], T[1], T[2]]))
     assert glom('abc', Template(lambda t: t.upper())) == 'ABC'
+    assert glom('a', 1) == 1
