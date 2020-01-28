6b74ce79715bcaed5b6f1c73c462c273453ba358
Kurt Rose <=>
2020-01-28 08:04:13 -0800

    added basic aggregation

--- glom/grouping.py
+++ glom/grouping.py
@@ -5,9 +5,19 @@ from boltons.typeutils import make_sentinel
 
 from .core import glom, MODE
 
+
 ACC = make_sentinel('ACC')
 ACC.__doc__ = """
 current accumulator for aggregation
+"""
+
+
+ACC2 = make_sentinel('ACC2')
+ACC2.__doc__ = """
+supporting accumulators for aggregation
+
+e.g. if current aggregation is average,
+the supporting data structure would be (sum, count)
 """
 
 
@@ -17,10 +27,14 @@ class Group(object):
 
     def glomit(self, target, scope):
         scope[MODE] = _group
-        scope[ACC] = type(self.spec)()  # dict or list
+        if type(self.spec) is list:
+            scope[ACC] = []
+        elif type(self.spec) is dict:
+            scope[ACC] = {}
+        scope[ACC2] = {}
         for t in target:
-            scope[glom](t, self.spec, scope)
-        return scope[ACC]
+            ret = scope[glom](t, self.spec, scope)
+        return ret
 
 
 def _group(target, spec, scope):
@@ -28,19 +42,25 @@ def _group(target, spec, scope):
     Group mode dispatcher
     """
     recurse = lambda spec: scope[glom](target, spec, scope)
-    acc = scope[ACC] # current accumulator
+    acc = scope[ACC]  # current accumulator
+    acc2 = scope[ACC2]  # current acuumulator support structure
     if type(spec) is dict:
         for keyspec, valspec in spec.items():
             key = recurse(keyspec)
             if key not in acc:
                 acc[key] = _mk_acc(valspec)
+                acc2[key] = {}
             scope[ACC] = acc[key]
-            recurse(valspec)
+            scope[ACC2] = acc2[key]
+            acc[key] = recurse(valspec)
+        return acc
     elif type(spec) is list:
         for valspec in spec:
             acc.append(recurse(valspec))
+        return acc
     elif callable(spec):
         return spec(target)
+    raise TypeError("not a valid spec")
 
 
 def _mk_acc(spec):
@@ -52,8 +72,14 @@ def _mk_acc(spec):
     if type(spec) is list:
         return []
 
-# need a function which grabs the parent spec and introspects
-# -- how to make a new child?
 
-# come to think of it -- a dict is the only key which is valid
+class First(object):
+    def __init__(self): pass
 
+    def glomit(self, target, scope):
+        if ACC2 not in scope:
+            raise Exception("called outside of aggregation scope")
+        acc2 = scope[ACC2]
+        if self not in acc2:
+            acc2[self] = target
+        return acc2[self]
--- glom/test/test_grouping.py
+++ glom/test/test_grouping.py
@@ -1,4 +1,4 @@
-from glom.grouping import Group
+from glom.grouping import Group, First
 from glom import glom, T
 
 
@@ -6,3 +6,8 @@ def test_bucketing():
     assert glom(range(4), Group({lambda t: t % 2 == 0: [T]})) == {True: [0, 2], False: [1, 3]}
     assert (glom(range(6), Group({lambda t: t % 3: {lambda t: t % 2: [lambda t: t / 10.0]}})) ==
         {0: {0: [0.0], 1: [0.3]}, 1: {1: [0.1], 0: [0.4]}, 2: {0: [0.2], 1: [0.5]}})
+
+
+def test_agg():
+	assert glom(range(10), Group(First())) == 0
+	assert glom(range(10), Group(T)) == 9  # this is basically Last
