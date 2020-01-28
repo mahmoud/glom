014ee623c038860fbfc79b9870ca71f68125ce73
Kurt Rose <kurt@kurtrose.com>
2020-01-27 18:01:19 -0800

    extended Group mode tests

--- glom/test/test_grouping.py
+++ glom/test/test_grouping.py
@@ -3,5 +3,6 @@ from glom import glom, T
 
 
 def test_bucketing():
-    assert glom([1, 2, 3, 4], Group({lambda t: t % 2 == 0: [T]})) == {True: [2, 4], False: [1, 3]}
-
+    assert glom(range(4), Group({lambda t: t % 2 == 0: [T]})) == {True: [0, 2], False: [1, 3]}
+    assert (glom(range(6), Group({lambda t: t % 3: {lambda t: t % 2: [lambda t: t / 10.0]}})) ==
+        {0: {0: [0.0], 1: [0.3]}, 1: {1: [0.1], 0: [0.4]}, 2: {0: [0.2], 1: [0.5]}})
