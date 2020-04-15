11f7044751fe3d4c1d6ba535c07f3a7eb34af9df
Martin Borus <martin@borus.de>
2020-04-15 19:20:46 +0200

    Added data driven example for tutorial.

--- glom/tutorial.py
+++ glom/tutorial.py
@@ -215,6 +215,33 @@ You can see here we get the expected results, but say our target changes...
 
 Voila, the target can still be parsed and we can elegantly handle changes in our data formats.
 
+Data-Driven Assignment
+======================
+
+Quite often APIs deliver data in dictionaries without constant key values.
+They use parts of the data itself as a key. This we call data-driven assignment.
+
+The following example shows you a way to handle this situation.
+It extracts the moon count from a dictionary that has the planet names as a key.
+
+  >>> from glom import glom, T, merge, Iter, Coalesce
+  >>> target = {
+  ...    "pluto": {"moons": 6, "population": None},
+  ...    "venus": {"population": {"aliens": 5}},
+  ...    "earth": {"moons": 1, "population": {"humans": 7_700_000_000, "aliens": 1}},
+  ... }
+  >>> spec = {
+  ...     "moons": (
+  ...          T.items(),
+  ...          Iter({T[0]: (T[1], Coalesce("moons", default=0))}).all(),
+  ...          merge,
+  ...     )
+  ... }
+  >>> pprint(glom(target, spec))
+  {'moons': {'pluto': 6, 'venus': 0, 'earth': 1}}
+
+Don't worry, if you do not fully understand how this works at this point. If you
+need it, look up ``Iter()``, ``T`` and ``merge`` in the glom API reference.
 
 True Python Native
 ==================
