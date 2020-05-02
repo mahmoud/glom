d69543cfdd7c7c96343ac2a69d41f74c8591167f
Mahmoud Hashemi <mahmoud@hatnote.com>
2020-05-02 06:50:47 -0700

    bump version for v20.5.1dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from setuptools import setup
 
 __author__ = 'Mahmoud Hashemi and Kurt Rose'
-__version__ = '20.5.0'
+__version__ = '20.5.1dev'
 __contact__ = 'mahmoud@hatnote.com'
 __url__ = 'https://github.com/mahmoud/glom'
 __license__ = 'BSD'
