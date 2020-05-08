ce522b619d4d78bf1db9dd94a2004911e5db8d97
yaroslavpankovych <flower.moor@gmail.com>
2020-05-08 17:07:43 +0300

    Added __version__ attribute.

--- glom/__init__.py
+++ glom/__init__.py
@@ -1,3 +1,4 @@
+from pkg_resources import get_distribution
 
 from glom.core import (glom,
                        Fill,
@@ -26,11 +27,11 @@ from glom.core import (glom,
                        CoalesceError,
                        UnregisteredTarget,
                        T, S)
-
-from glom.reduction import Sum, Fold, Flatten, flatten, FoldError, Merge, merge
 from glom.mutation import Assign, Delete, assign, delete, PathAssignError, PathDeleteError
-
+from glom.reduction import Sum, Fold, Flatten, flatten, FoldError, Merge, merge
 # there's no -ion word that really fits what "streaming" means.
 # generation, production, iteration, all have more relevant meanings
 # elsewhere. (maybe procrastination :P)
 from glom.streaming import Iter
+
+__version__ = get_distribution(dist="glom").version
