43db1d704a470f682af6e717695ef2a13920a53b
Kurt Rose <=>
2020-06-27 19:36:37 -0700

    added callable escape valve to match

--- glom/matching.py
+++ glom/matching.py
@@ -676,6 +676,10 @@ def _glom_match(target, spec, scope):
         for sub_target, sub_spec in zip(target, spec):
             result.append(scope[glom](sub_target, sub_spec, scope))
         return tuple(result)
+    elif callable(spec):
+        if spec(target):
+            return target
+        raise MatchError("{}({!r}) did not validate".format(spec.__name__, target))
     elif target != spec:
         raise MatchError("{!r} does not match {!r}", target, spec)
     return target
--- glom/test/test_match.py
+++ glom/test/test_match.py
@@ -29,6 +29,7 @@ def test_basic():
     _chk(Match([int]), [1], ["1"])
     _chk(Match({int}), {1}, [1])
     _chk(Match(frozenset({float})), frozenset({}), frozenset({"1"}))
+    _chk(Match(len), [1], [])
     with pytest.raises(MatchError):
         glom([1], Match([]))  # empty shouldn't match
     glom({"a": 1, "b": 2}, Match({str: int}))
