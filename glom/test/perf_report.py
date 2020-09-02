40d1945a62f170b9191d03ab57c34e59e3d90e3e
Kurt Rose <=>
2020-09-01 20:10:00 -0700

    working on perf report

--- glom/test/perf_report.py
+++ glom/test/perf_report.py
@@ -2,6 +2,7 @@
 slow gloms that came up organically, used as performance metrics
 """
 import time
+import gc
 
 import attr
 
@@ -25,6 +26,15 @@ T_SPEC = [{
 }]
 
 
+def func(data):
+    return [{
+            'id': str(t.id),
+            'name': t.short_name,
+            'external_id': t.external_id,
+            'created_date': t.created_date
+        } for t in data]
+
+
 def setup_list_of_dict(num=100):
     """
     a common use case is list-of-dicts object processing
@@ -46,7 +56,28 @@ def run(spec, data):
     print("{} us per object".format((end - start) / len(data) * 1e6))
 
 
+def ratio(spec, func, data):
+    glom_dur = []
+    py_dur = []
+    for i in range(10):
+        t1 = time.perf_counter_ns()
+        glom(data, spec)
+        t2 = time.perf_counter_ns()
+        func(data)
+        t3 = time.perf_counter_ns()
+        glom_dur.append(t2 - t1)
+        py_dur.append(t3 - t2)
+
+    glom_avg = sum(sorted(glom_dur)[2:-2])
+    py_avg = sum(sorted(py_dur)[2:-2])
+
+    return 1.0 * glom_avg / py_avg
+
+
 if __name__ == "__main__":
     import cProfile
     data = setup_list_of_dict(10000)
-    cProfile.run('run(STR_SPEC, data)')
+    run(STR_SPEC, data)
+    run(STR_SPEC, data)
+    print(ratio(STR_SPEC, func, setup_list_of_dict(1000)))
+    print(ratio(STR_SPEC, func, setup_list_of_dict(1000)))
