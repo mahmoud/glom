eeb48b774f08fea7f0160842d5dd828e93914a34
Mahmoud Hashemi <mahmoud@hatnote.com>
2022-01-24 01:34:41 -0800

    bump docs version

--- docs/conf.py
+++ docs/conf.py
@@ -36,9 +36,9 @@ copyright = u'2020, Mahmoud Hashemi'
 author = u'Mahmoud Hashemi'
 
 # The short X.Y version
-version = u'20.11'
+version = u'22.1'
 # The full version, including alpha/beta/rc tags
-release = u'20.11.0'
+release = u'22.1.0'
 
 
 todo_add_to_theme_to_keep_menus_expanded = """
