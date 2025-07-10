a45f722240c84e4f43541c34da88179518dc8820
Branch Vincent <branchevincent@gmail.com>
2025-07-09 20:48:06 -0700

    migrate to pyproject.toml

--- glom/_version.py
+++ glom/_version.py
@@ -1,2 +1,4 @@
-version_info = (24, 11, 1, 'dev')
-__version__ = '.'.join([str(part) for part in version_info if part or part == 0])
+__version__ = "24.11.1.dev"
+version_info = tuple(
+    int(part) if part.isdigit() else part for part in __version__.split(".")
+)
